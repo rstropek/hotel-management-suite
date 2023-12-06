ca34b9d61a9738c6f0e2da72a9b6cbad956b9f4e
Rainer <rainer@software-architects.at>
2023-12-06 15:24:45 +0100

    Add DB access

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
+
 app.MapGet("/ping", () => "pong");
+
+app.MapGet("/get-from-db", async (IConfiguration configuration) =>
+{
+    using var conn = new SqlConnection(configuration.GetConnectionString("Database"));
+    await conn.OpenAsync();
+    using var cmd = conn.CreateCommand();
+    cmd.CommandText = "SELECT 42 AS [Answer]";
+    return Results.Ok(await cmd.ExecuteScalarAsync());
+});
+
 app.Run();
